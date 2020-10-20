a89ba424cb2705e3c508a7f3cfe449daaa7afe3a
Maks <maks1makrov@gmail.com>
2020-10-20 20:10:33 +0300

    ADD for linux

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -79,6 +79,17 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 #
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_base_book',
+        'USER': 'book_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -89,15 +100,15 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 #         'PORT': 5432
 #     }
 # }
-#
-# SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
-    }
-}
+SOCIAL_AUTH_POSTGRES_JSONFIELD = True
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
 
 
 # Password validation
